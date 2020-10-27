f48e71e8b09aeab74283a29eccb9ef1a11aa5abc
Mai Pham <maitphm@gmail.com>
2020-10-27 19:10:27 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,9 @@ def find_element_index(array, value_to_find)
   while i < array.length do 
     if array[i] == value_to_find 
       found_value_index = i 
+    end 
     i += 1 
+  
   end 
-  return found_value_index
+  found_value_index
 end
